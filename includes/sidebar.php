f8a2fa0d6e5e839a52b0bec6904a767232793023
capitale250 <capkrypton@gmail.com>
2022-08-02 19:23:54 +0200

    upload file

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -135,4 +135,11 @@
 
     </ul>
     <!-- End of Sidebar -->
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.slim.min.js"></script>
+
+<!-- Popper JS -->
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
     <?php include_once 'topbar.php'; ?>
