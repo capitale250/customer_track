3d31ffde3ffe40d0b897c9b5eadb1545f79b5dca
capitale250 <capkrypton@gmail.com>
2022-08-03 02:40:53 +0200

    upload file

--- includes/topbar.php
+++ includes/topbar.php
@@ -54,14 +54,14 @@
 
               <!-- Dropdown - User Information -->
               <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
-                <button class="dropdown-item" onclick="on()">
+                <!-- <button class="dropdown-item" onclick="on()">
                   <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
                   Profile
-                </button>
-                <a class="dropdown-item" href="settings.php?action=edit & id='<?php echo $a; ?>'">
+                </button> -->
+                <!-- <a class="dropdown-item" href="settings.php?action=edit & id='<?php echo $a; ?>'">
                   <i class="fas fa-cogs fa-sm fa-fw mr-2 text-gray-400"></i>
                   Settings
-                </a>
+                </a> -->
                 <div class="dropdown-divider"></div>
                 <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
                   <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
