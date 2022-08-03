5623fceb65abebe21e265305f640d3dcc7831236
capitale250 <capkrypton@gmail.com>
2022-08-03 02:51:21 +0200

    upload file

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -49,7 +49,7 @@
 
   <!-- Custom styles for this page -->
   <link href="../vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 </head>
 
 <body id="page-top">
--- pages/product.php
+++ pages/product.php
@@ -48,7 +48,7 @@ $sup .= "</select>";
             </div>
             <div class="card-body">
               <div class="table-responsive">
-                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0"> 
+                <table class="table table-striped table-bordered table-sm" id="dataTable" width="100%" cellspacing="0"> 
                <thead>
                    <tr>
                      <!-- <th>Product Code</th> -->
